45c75363a52f68feeefc04d27a3a8d41c2d88146
Blake Hudson <blakehudson615@gmail.com>
2021-09-02 12:35:45 -0500

    Committing first version of working code for index.js and filled out README.md

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 const validatePassword = require('./passwordvalidation'); //import the function
+const readline = require('readline');   //readline module for reading datastream
 
-// Your code goes here!
+
+//creates readline interface using stdin/out
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+//prompts user to enter a password and passes input to validation function until quit string is entered
+rl.question('Enter a password for validation (or :q to quit) ', (answer) => {
+
+    if ((answer) != 'q') {
+        passwordFeedback(answer);
+        rl.on('line', (answer) => {
+            if (answer != 'q') {
+                passwordFeedback(answer);
+
+            } else {
+                closeRl();
+                return;
+            }
+        });
+    }else{  //needed to close if 'q' is first input received
+        closeRl();
+        return;
+    }
+});
+
+//Function gives user feedback on validity of entered password
+//Uses validatePassword() to return boolean
+function passwordFeedback(word) {
+
+
+    if (validatePassword(word)) {
+        console.log('The password is valid: ', (word));
+    }
+    else {
+        console.log('The password is invalid: ', (word));
+
+    }
+}
+
+//function for closing the rl readline interface and displays exit message to user
+function closeRl() {
+    console.log('Goodbye!');
+    rl.close();
+}
+
+
+
